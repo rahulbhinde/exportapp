bd560670679c250ff24d6894f27c4f0931f50044
rahulbhinde <32032708+rahulbhinde@users.noreply.github.com>
2026-04-16 10:10:36 +0530

    Add combined_files.sql for user interfaces

---
+++ Application/application/user_interfaces/combined_files.sql
@@ -0,0 +1,17 @@
+prompt --application/user_interfaces/combined_files
+begin
+--   Manifest
+--     COMBINED FILES: 104
+--   Manifest End
+wwv_flow_imp.component_begin (
+ p_version_yyyy_mm_dd=>'2024.11.30'
+,p_release=>'24.2.15'
+,p_default_workspace_id=>7965342552622829
+,p_default_application_id=>200
+,p_default_id_offset=>32162224829641601
+,p_default_owner=>'SAARTHIDEV'
+);
+null;
+wwv_flow_imp.component_end;
+end;
+/
